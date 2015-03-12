e892da5e50ad7fd1686d3f6af40fcb9cc9e0d72d
mattheweallen <mattheweallen@gmail.com>
2015-03-12 06:11:27 -0500

    added attributes for user

--- src/Pequam.Data/Entities/User.cs
+++ src/Pequam.Data/Entities/User.cs
@@ -8,6 +8,11 @@ namespace Pequam.Data.Entities
 {
     class User
     {
-        //blrakfjdsl test
-    }
+        public virtual long UserId { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string UserName { get; set; }
+        public virtual string EmailAddress { get; set; }
+        public virtual byte[] Version { get; set; }
+     }
 }

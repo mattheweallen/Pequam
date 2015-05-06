ef7cf29ad32d81ee2dfd1983233dc231b1968d83
unknown <irgepi@LCO-TL-C609L32.corp.irco.com>
2015-05-06 07:36:29 -0500

    more auto mapper and start add challenge

---
+++ src/Pequam.Web.Api/MaintenanceProcessing/IAddChallengeMaintenanceProcessor.cs
@@ -0,0 +1,9 @@
+﻿using Pequam.Web.Api.Models;
+
+namespace Pequam.Web.Api.MaintenanceProcessing
+{
+    public interface IAddChallengeMaintenanceProcessor
+    {
+        Challenge AddChallenge(NewChallenge newChallenge);
+    }
+}
